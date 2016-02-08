e72ebfd8fb62a697ae58f96583ef49fde53239e2
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-02-09 00:37:53 +0200

    Add id.

--- model/user.js
+++ model/user.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        first: String,
-        last: String
-    },
+    id: Number,
+    firstName: String,
+    lastName: String,
     accessToken: String
 });
 
