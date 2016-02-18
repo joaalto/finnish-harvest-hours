5e11f6c1dc11d3d1fa895df057d9b0cc584360c1
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-02-18 23:12:19 +0200

    Some error handling and fetching date entries.

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const Promise = this.Promise || require('promise');
 const agent = require('superagent-promise')(require('superagent'), Promise);
 
-function user(req) {
+function getUser(req) {
     return req.session.passport.user;
 };
 
@@ -13,31 +13,49 @@ const baseUrl = 'https://wunderdog.harvestapp.com';
 module.exports = {
 
     get(req, url) {
-            return agent.get(baseUrl + url)
-                .type('json')
-                .accept('json')
-                .query({
-                    access_token: user(req).accessToken
-                })
-                .end();
-        },
-
-        projects(req) {
-            const proj = this.get(req, '/projects')
-                .then((resp) => {
-                    return _.map(resp.body, (row) => {
-                        // console.log('row:', row.project.id);
-                        return row.project.id;
-                    })
-                });
-            console.log('proj', proj);
-        },
-
-        dayEntries(req) {
-            _.forEach(this.projects, (project) => {
-                this.get(
-                    req,
-                    `/projects/${project.id}/entries?from=${startDate}&to=${endDate}&user_id=${user(req).id}`);
+        return promise = agent.get(baseUrl + url)
+            .type('json')
+            .accept('json')
+            .query({
+                access_token: getUser(req).accessToken
+            })
+            .end()
+            .then(resp => resp.body)
+            .catch(err => {
+                if (err.response.status === 401) {
+                    // TODO: login again, add ttl to session
+                    req.session.destroy();
+                }
+                console.error('error:', err.response);
             });
-        }
+    },
+
+    projects(req) {
+        const proj = this.get(req, '/projects')
+            .then(projects => _.map(projects, row => row.project.id))
+            .then(projectIds => this.dayEntries(req, projectIds));
+
+        console.log('proj:', proj);
+    },
+
+    dayEntries(req, projectIds) {
+        return Promise.all(_.forEach(projectIds, projectId => {
+                return this.get(
+                        req,
+                        `/projects/${projectId}/entries?from=${startDate}&to=${endDate}&user_id=${getUser(req).id}`)
+                    .then(entries => {
+                        return _.map(entries, entry => {
+                            return {
+                                date: entry.spent_at,
+                                hours: entry.hours
+                            };
+                        });
+                    })
+                    .catch(err => console.error('>>> error:', err));
+            })).then(res => {
+                console.log('>>> res', res);
+                return res;
+            })
+            .catch(err => console.error('ERR:', err));
+    }
 };
