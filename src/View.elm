506fc45c40a68f1a579a124c34f07e25312a74e8
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-02-21 23:55:31 +0200

    Format.

--- src/View.elm
+++ src/View.elm
@@ -1,21 +1,22 @@
-module View where
+module View (..) where
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Signal exposing (Address)
-
 import Model exposing (..)
 import Update exposing (..)
 
+
 view : Address Action -> Model -> Html
 view address model =
-    case model.httpError of
-        Err err ->
-            div
-                [ style [ ("color", "red" ) ]]
-                [ text (toString err) ]
-        Ok _ ->
-            div
-                []
-                [ text "Hello, world." ]
+  case model.httpError of
+    Err err ->
+      div
+        [ style [ ( "color", "red" ) ] ]
+        [ text (toString err) ]
+
+    Ok _ ->
+      div
+        []
+        [ text "Hello, world." ]
