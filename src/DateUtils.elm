655ae18da3cca3914a8d9a116fc0160809bbd8cf
Joonas Aalto <joonas.aalto@wunderdog.fi>
2016-02-27 14:58:46 +0200

    Implement date comparison.

--- src/DateUtils.elm
+++ src/DateUtils.elm
@@ -5,7 +5,9 @@ import Date exposing (..)
 import Date.Core exposing (..)
 import Date.Utils exposing (..)
 import Date.Period as Period exposing (add, diff)
-import Date.Compare exposing (is, Compare2)
+import Date.Compare as Compare exposing (is, Compare2)
+import Date.Format exposing (isoString)
+import Date.Floor as Df exposing (floor)
 
 
 -- totalHoursForYear : Date -> Float
@@ -18,11 +20,22 @@ totalDaysForYear currentDate =
 
 workDays : Date -> Date -> List Date -> List Date
 workDays date currentDate days =
-  if (is Date.Compare.Same currentDate date) then
+  if isSameDate date currentDate then
     days
   else
     let
       nextDay =
         add Period.Day 1 date
+
+      isoStr =
+        (Debug.log "nextDay" (isoString nextDay))
     in
-      workDays (Debug.log "nextDay" nextDay) currentDate (nextDay :: days)
+      workDays nextDay currentDate (nextDay :: days)
+
+
+isSameDate : Date -> Date -> Bool
+isSameDate date1 date2 =
+  is
+    Compare.Same
+    (Df.floor Df.Day date1)
+    (Df.floor Df.Day date2)
