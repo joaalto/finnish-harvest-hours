84ea889d09497443160bc1f8f4dd305be001b7a3
Patrik Johnson <patrik.johnson@wunderdog.fi>
2019-11-07 15:48:25 +0200

    Add formatting helper for converting Dates to Strings, and related tests

--- tests/FormattingTest.elm
+++ tests/FormattingTest.elm
@@ -3,12 +3,15 @@ module FormattingTest exposing (all)
 import Test exposing (..)
 import Expect
 import Round exposing (round)
-import Formatting exposing (floatToHoursAndMins)
+import Date exposing (Month(..), Date)
+import Date.Extra.Create exposing (dateFromFields)
+import Formatting exposing (floatToHoursAndMins, dateToString)
 
 
 all : Test
 all =
-    describe "Hour formatting"
+    describe "Formatting"
+    [ describe "Hour formatting"
         [ test "Format Int to String with two decimals" <|
             \() ->
                 Round.round 2 5
@@ -26,3 +29,12 @@ all =
             \() ->
                 floatToHoursAndMins (Just 0.21) |> Expect.equal "0:13"
         ]
+    , describe "Date formatting"
+        [ test "Formats date to String" <|
+            \() ->
+                dateToString (Just (dateFromFields 2017 Mar 27 23 59 0 0)) |> Expect.equal "27.03.2017"
+        , test "Formats nothing to infinity symbol" <|
+            \() ->
+                dateToString Nothing |> Expect.equal "∞"
+        ]
+    ]
